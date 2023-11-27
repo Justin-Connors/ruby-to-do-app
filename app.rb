86e39d6095e79e1aee99942cdea5f21d3bdc0cdf
justin connors <justinconnors@hotmail.ca>
2023-11-27 14:36:38 -0400

    can delete a task, and mark a task as complete

--- app.rb
+++ app.rb
@@ -1,16 +1,32 @@
 require 'sinatra'
 require 'erb'
 
+#Store tasks in an array
 tasks = []
 
+#default route
 get '/' do
     @tasks = tasks
     erb :index
 end
 
+#add task route
 post '/add_task' do
     task = params[:task]
     tasks << task unless task.empty?
     redirect '/'
 end
 
+#delete task route
+post '/delete_task' do
+    task_id = params[:task_id].to_i
+    tasks.delete_at(task_id)
+    redirect '/'
+end
+
+#complete task route
+post '/complete_task' do
+    task_id = params[:task_id].to_i
+    tasks[task_id] = "<strike>#{tasks[task_id]}</strike>"
+    redirect '/'
+end
